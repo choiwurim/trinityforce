5266c6277e53f235eebfa3db1e649cb19052026c
woorim <91938990+choiwurim@users.noreply.github.com>
2022-02-03 16:50:43 +0900

    Add files via upload

---
+++ datastructure/Node.java
@@ -0,0 +1,14 @@
+import java.util.*;
+package list;
+public class Node<E> {
+	public E item;
+	public Node<E> next;
+	public Node(E newItem) {
+		item=newItem;
+		next=null;
+	}
+	public Node(E newItem, Node<E> nextNode) {
+		item=newItem;
+		next=nextNode;
+	}
+}
